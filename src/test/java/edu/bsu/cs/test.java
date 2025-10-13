87abbe5d437f7193170cc511daeb61b557966dc7
Caleb L <celangley917@gmail.com>
2025-10-13 17:02:24 -0400

    Initial Commit

---
+++ src/test/java/edu/bsu/cs/test.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class test {
+    @Test
+    public void test(){
+        System.out.println("test");
+        Assertions.assertTrue(true);
+    }
+}
