3db0aedb0d3dab0f0cb844afb4f9d09672b280f0
willmcroberts <williammcroberts15@gmail.com>
2025-11-06 11:37:42 -0500

    Add  citation commit to ConfigLoader.java

--- src/main/java/edu/bsu/cs/ConfigLoader.java
+++ src/main/java/edu/bsu/cs/ConfigLoader.java
@@ -4,6 +4,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/*
+This file and other bits of code found in the SteamConnection.java at the top
+were constructed using a tutorial on "Medium", while taking into the top comment
+advising to put the key in a new file not the gradle.properties
+ */
+
 public class ConfigLoader {
     private static final Properties props = new Properties();
 
