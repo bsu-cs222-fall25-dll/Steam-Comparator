fe1184d8981f4dceb8a1e51e18b08752c59e9dd9
willmcroberts <williammcroberts15@gmail.com>
2025-11-11 11:28:09 -0500

    Make GUI work, Add "Enter All" button, Code working

--- src/main/java/edu/bsu/cs/GUI.java
+++ src/main/java/edu/bsu/cs/GUI.java
@@ -2,10 +2,7 @@ package edu.bsu.cs;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -19,69 +16,45 @@ public class GUI extends Application {
         launch(args);
     }
 
-    private TextField inputField = new TextField();
-    private TextArea outputField = new TextArea();
+    private TextField inputField1;
+    private TextArea outputField1;
+    private TextField inputField2;
+    private TextArea outputField2;
 
 
     @Override
     public void start(Stage primaryStage) {
-        Scene scene = new Scene(new Label("SteamComparator"));
+        primaryStage.setTitle("SteamComparator");
+        Pane root = createRoot();
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
         primaryStage.show();
-        outputField.setEditable(false);
-        configure(primaryStage);
-        configureInputField();
-    }
-
-    private void configure(Stage stage){
-        stage.setTitle("SteamComparator");
-        stage.setScene(new Scene(createRoot()));
-        stage.sizeToScene();
-        stage.show();
-    }
-    private void configureInputField() {
-        inputField.setOnAction((ActionEvent) ->  {
-            try {
-                displayUser();
-            } catch (IOException | URISyntaxException e) {
-                displayError("Network Connection Issue");
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
     }
 
     private Pane createRoot() {
-        VBox leftColumn = new VBox(5);
-        TextField inputField1 = new TextField();
-        TextArea outputField1 = new TextArea();
+        inputField1 = new TextField();
+        outputField1 = new TextArea();
         outputField1.setEditable(false);
+
+        VBox leftColumn = new VBox(5);
         leftColumn.getChildren().addAll(
                 new Label("Steam Link:"), inputField1,
                 new Label("Information:"), outputField1
         );
+        
+        inputField2 = new TextField();
+        outputField2 = new TextArea();
+        outputField2.setEditable(false);
 
         VBox rightColumn = new VBox(5);
-        TextField inputField2 = new TextField();
-        TextArea outputField2 = new TextArea();
-        outputField2.setEditable(false);
         rightColumn.getChildren().addAll(
                 new Label("Steam Link:"), inputField2,
                 new Label("Information:"), outputField2
         );
 
-        HBox root = new HBox(10);
-        root.getChildren().addAll(leftColumn, rightColumn);
+        return getHBox(leftColumn, rightColumn);
 
-        root.setPrefSize(800, 400);
-
-        this.inputField = inputField1;
-        this.outputField = outputField1;
-
-        this.inputField = inputField2;
-        this.outputField = outputField2;
-
-        return root;
     }
 
     private HBox getHBox(VBox leftColumn, VBox rightColumn) {
@@ -106,16 +79,19 @@ public class GUI extends Application {
         String steamLink =  inputField.getText().trim();
         if (steamLink.isEmpty()){
             displayError("Nothing entered");
+            return;
         }
+
         String accountName = AccountParser.parseAccountName(steamLink);
         String userData = UserFetcher.getUserDataAsString(accountName);
         String gameData = UserFetcher.getGameDataAsString(accountName);
         User user = UserParser.parseUserData(userData, gameData);
         outputField.setText(user.printUser());
-        String output = outputField.getText().trim();
-        if(output.isEmpty()){
+
+        if (outputField.getText().trim().isEmpty()) {
             displayError("No article found");
         }
+
     }
 
     static void displayError(String message){
