ed19f0e4c21bac6f9361fcda25a91d7fbe972cc7
willmcroberts <williammcroberts15@gmail.com>
2025-11-18 13:21:37 -0500

    Change game hours format
    Changed the formatting of the hours played when printing the hours played for each game to fit the format that Steam uses

--- src/main/java/edu/bsu/cs/Game.java
+++ src/main/java/edu/bsu/cs/Game.java
@@ -2,6 +2,6 @@ package edu.bsu.cs;
 
 public record Game(int minutes, int appID, String gameName) {
     public String printGame() {
-        return (gameName() + ": " + String.format("%.2f", minutes() / 60.0) + " hours");
+        return (gameName() + ": " + String.format("%.1f", minutes() / 60.0) + " hours");
     }
 }
