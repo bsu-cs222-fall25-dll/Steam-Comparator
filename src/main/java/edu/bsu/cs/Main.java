f1952fd3e04ee6c5ce6d20e6fb5bbe7f8527f408
lukethelehman <lukelehman05@gmail.com>
2025-11-24 12:46:07 -0500

    Refactor GUI.fxml to resources folder. Update controller reference

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -1,8 +1,21 @@
 package edu.bsu.cs;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        CommandLine.run();
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/GUI.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("SteamComparator");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
