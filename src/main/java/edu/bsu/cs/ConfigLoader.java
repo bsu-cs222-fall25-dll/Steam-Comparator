50bba15bd91fa0845d37d98126330ebcdfed79b3
willmcroberts <williammcroberts15@gmail.com>
2025-11-29 14:45:47 -0500

    Remove note

--- src/main/java/edu/bsu/cs/ConfigLoader.java
+++ src/main/java/edu/bsu/cs/ConfigLoader.java
@@ -4,12 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-/*
-This file and other bits of code found in the SteamConnection.java at the top
-were constructed using a tutorial on "Medium", while taking into the top comment
-advising to put the key in a new file not the gradle.properties
- */
-
 public class ConfigLoader {
     private static final Properties props = new Properties();
 
