01f0c1983ea979e2b8a38feb31b190c4e2d0848b
lukethelehman <lukelehman05@gmail.com>
2025-12-01 15:05:43 -0500

    refactor GUI.fxml to resources folder, delete irrelevant command line/old main

---
+++ src/main/java/edu/bsu/cs/GUIController.java
@@ -0,0 +1,64 @@
+package edu.bsu.cs;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class GUIController {
+
+    @FXML
+    private TextField inputField1;
+    @FXML
+    private TextArea outputField1;
+    @FXML
+    private TextField inputField2;
+    @FXML
+    private TextArea outputField2;
+    @FXML
+    private ComboBox<String> compareOptions;
+    @FXML
+    private ComboBox<Integer> amountOfGames;
+
+    @FXML
+    public void initialize() {
+        compareOptions.getItems().addAll("Most hours", "Recent hours");
+        compareOptions.setValue("Most hours");
+
+        amountOfGames.getItems().addAll(5, 10, 15);
+        amountOfGames.setValue(10);
+    }
+
+    @FXML
+    private void handleCompareAction() {
+        try {
+            displayUser(inputField1, outputField1);
+            displayUser(inputField2, outputField2);
+        } catch (Exception ex) {
+            displayError(ex.getMessage());
+        }
+    }
+
+    private void displayUser(TextField inputField, TextArea outputField) throws Exception {
+        String steamLink = inputField.getText().trim();
+        if (steamLink.isEmpty()) {
+            displayError("Nothing entered");
+            return;
+        }
+
+        String accountName = AccountParser.parseAccountName(steamLink);
+        String userData = UserFetcher.getUserDataAsString(accountName);
+        String gameData = UserFetcher.getOwnedGamesAsString(accountName);
+        String recentData = UserFetcher.getRecentlyPlayedDataAsString(accountName);
+        User user = UserParser.parseUserData(userData, gameData, recentData);
+        outputField.setText(user.printUser());
+
+        if (outputField.getText().trim().isEmpty()) {
+            displayError("User not found");
+        }
+    }
+
+    private void displayError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -2,20 +2,29 @@ package edu.bsu.cs;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/GUI.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setScene(scene);
-        stage.setTitle("SteamComparator");
-        stage.show();
-    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL fxmlLocation = getClass().getResource("/edu/bsu/cs/GUI.fxml");
+        FXMLLoader loader = new FXMLLoader(fxmlLocation);
+        Parent root = loader.load();
+
+        primaryStage.setTitle("SteamComparator");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
     }
 }
