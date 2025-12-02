bd35f62adfc3a8ce35e181d3e2c6c3ab325661a7
lukethelehman <lukelehman05@gmail.com>
2025-12-02 10:00:51 -0500

    fix gradle build to specify java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+
 plugins {
     id("java")
     id("application")
@@ -9,12 +11,18 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.slf4j:slf4j-nop:2.0.11")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
-    implementation("net.minidev:json-smart:2.5.0")
+    implementation("net.minidev:json-smart:2.5.2")
 }
 tasks.test {
     useJUnitPlatform()
@@ -24,5 +32,5 @@ javafx {
     modules("javafx.controls", "javafx.fxml")
 }
 application {
-    mainClass.set("edu.bsu.cs.GUI")
+    mainClass.set("edu.bsu.cs.Main")
 }
